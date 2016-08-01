58695711cc6787de19ccb1a9a538386a8c8aacd8
stevengoo <stevenngo36@yahoo.com>
2016-08-01 11:45:34 -0700

    Added Random data generation, and other optimizations

--- micro-controller/TTN/ttn_working/ttn_working.ino
+++ micro-controller/TTN/ttn_working/ttn_working.ino
@@ -46,7 +46,7 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "Hello, world!";
+//static uint8_t mydata[] = "Hello, world!";
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
@@ -103,6 +103,7 @@ void onEvent (ev_t ev) {
         Serial.write(LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
         Serial.println();
       }
+      delay(10000);
       // Schedule next transmission
       os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
       break;
@@ -129,12 +130,28 @@ void onEvent (ev_t ev) {
 }
 
 void do_send(osjob_t* j) {
+  byte sendLen;
+  int temp = random(50,100); //range of 50 to 100
+  int humi = random(100);    //max of 100
+  int wet = random(20);      //max of 20
+  char buffer[255]; //final byte array that gets passed to radio.send
+  sendLen = strlen(buffer);  //get the length of buffer
+  //static uint8_t mydata[] = "HELLO WORLD";
+  sprintf(buffer, "NODEID:03FFEBB2 L:%d F:%d H:%d",
+         // NODEID,
+          wet, //getLeafWetness()
+          temp,//getFahrenheitHundredths() 
+          humi//getHumidityPercent()
+          //Will later implement functions to get data from sensors
+          );
+  
   // Check if there is not a current TX/RX job running
   if (LMIC.opmode & OP_TXRXPEND) {
     Serial.println(F("OP_TXRXPEND, not sending"));
   } else {
     // Prepare upstream data transmission at the next possible time.
-    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    LMIC_setTxData2(1, (uint8_t *)buffer, sendLen, 0);
+    //LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
     Serial.println(F("Packet queued"));
   }
   // Next TX is scheduled after TX_COMPLETE event.
@@ -172,28 +189,6 @@ void setup() {
   LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
 #endif
 
-  // Set up the channels used by the Things Network, which corresponds
-  // to the defaults of most gateways. Without this, only three base
-  // channels from the LoRaWAN specification are used, which certainly
-  // works, so it is good for debugging, but can overload those
-  // frequencies, so be sure to configure the full frequency range of
-  // your network here (unless your network autoconfigures them).
-  // Setting up channels should happen after LMIC_setSession, as that
-  // configures the minimal channel set.
-  //    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF10, DR_SF7B), BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF10, DR_SF7),  BAND_CENTI);      // g-band
-  //    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-  // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-  // devices' ping slots. LMIC does not have an easy way to define set this
-  // frequency and support for class B is spotty and untested, so this
-  // frequency is not configured here.
-
   //disable all channels outside of sub-band 7
   //http://forum.thethingsnetwork.org/t/arduino-uno-dragino-shield-v1-3-code/2647/5
   for (int channel = 0; channel < 48; ++channel) {
@@ -209,7 +204,9 @@ void setup() {
   // Set data rate and transmit power (note: txpow seems to be ignored by the library)
   //LMIC_setDrTxpow(DR_SF7,14);
   LMIC_setDrTxpow(DR_SF9, 14);
-
+  
+  randomSeed(analogRead(0)); //Generate seed based off of analog 0
+  Serial.print("starting");
   // Start job
   do_send(&sendjob);
 }
